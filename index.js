0a302a939a571dd23d4c839bd472ddeb8dc4efb3
antonioalmeida <theantonioalmeida@gmail.com>
2019-10-15 21:54:27 +0200

    fix 'db' filename

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
-const app = express()
+const jsonServer = require('json-server')
+const router = jsonServer.router('ditados.json')
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
  
-app.get('/', function (req, res) {
+server.use(router)
+server.use(middlewares)
+
+server.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+server.listen(3000)
