d61ae4f400ab8fe2a7fe807165f7046bbb8c47d1
António Almeida <theantonioalmeida@gmail.com>
2019-10-22 18:52:37 +0200

    Setup Github Actions' workflow for base CI and CD (#2)
    * install eslint for sample github action
    
    * add lint script and run it
    
    * create base node action
    
    * add base Heroku procfile (do I need this?)
    
    * add running server log
    
    * move json-server from dev to regular dependency
    
    * add dynamic port selection
    
    * fix previous commit
    
    * testing heroku action with login
    
    * testing again
    
    * is it args (?)
    
    * login worked, trying push
    
    * moving heroku ci to different file
    
    * trying workflow file instead of yml
    
    * move main.workflow to workflow dir
    
    * go back to yml - hcl is deprecated
    
    * setup porting according to heroku's requirements
    
    * change shell commands separation on action
    
    * separate heroku steps
    
    * structure workflow file to fix issues
    
    * add checkout step to deploy workflow

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express')
 const jsonServer = require('json-server')
 const router = jsonServer.router('ditados.json')
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
+
+const PORT = process.env.PORT || 3000;
  
 server.use(router)
 server.use(middlewares)
@@ -11,4 +12,6 @@ server.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-server.listen(3000)
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
