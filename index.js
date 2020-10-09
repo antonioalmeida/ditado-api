c275c0ed28d1cc4ac43205656292c7e84b64de7b
enzomzaocv <38295665+enzomzaocv@users.noreply.github.com>
2020-10-09 12:23:50 -0300

    added random endpoint (#26)

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const jsonServer = require('json-server')
 const router = jsonServer.router('ditados.json')
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
-
+const random = require('./random')
+const ditadosArray = require('./ditados.json').ditados;
+// PORT
 const PORT = process.env.PORT || 3000;
- 
-server.use(router)
+//Middlewares
 server.use(middlewares)
 
+// Custom Routes
 server.get('/', function (req, res) {
   res.send('Hello World')
+});
+// Return random ditado.
+server.get('/ditados/random', function (req, res) {
+  const ditado = random.getRandomDitado(false,ditadosArray);
+  res.status(200).send(ditado)
 })
- 
+
+
+// 
+server.use(router)
+// Listen
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
