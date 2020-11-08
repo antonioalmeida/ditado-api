d033731e61799df9594e6f90b4928478aed51477
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-11-08 00:58:44 +0000

    Proper segregation of source code and tests (#44)

---
+++ test/random.test.js
@@ -0,0 +1,18 @@
+const random = require('../random')
+const { expect, test } = require('@jest/globals');
+const ditadosArray = require('../ditados.json').ditados;
+
+test('getting all ditados one by one using random should return no duplicate', () => {
+    const seenDitados = [];
+    let foundDuplicateDitado = false;
+    const totalDitados = ditadosArray.length;
+    for(let i = 0; i < totalDitados; i++){
+        const ditado = random.getRandomDitado(false, ditadosArray);
+        if(ditado in seenDitados) {
+            foundDuplicateDitado = true;
+            break;
+        }
+        seenDitados.push(ditado);
+    }
+    expect(foundDuplicateDitado).toBe(false);
+});
