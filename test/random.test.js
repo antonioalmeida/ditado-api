1a576c9393ffb3f916b91f38022b2d68a64f252f
César Magallón <cesar9mv@gmail.com>
2021-11-01 13:36:16 -0600

    Add eslint support (#49)
    * add eslint support
    
    * fix eslint warnings

--- test/random.test.js
+++ test/random.test.js
@@ -1,18 +1,18 @@
+const { expect, test } = require('@jest/globals')
 const random = require('../random')
-const { expect, test } = require('@jest/globals');
-const ditadosArray = require('../ditados.json').ditados;
+const ditadosArray = require('../ditados.json').ditados
 
 test('getting all ditados one by one using random should return no duplicate', () => {
-    const seenDitados = [];
-    let foundDuplicateDitado = false;
-    const totalDitados = ditadosArray.length;
-    for(let i = 0; i < totalDitados; i++){
-        const ditado = random.getRandomDitado(false, ditadosArray);
-        if(ditado in seenDitados) {
-            foundDuplicateDitado = true;
-            break;
-        }
-        seenDitados.push(ditado);
+  const seenDitados = []
+  let foundDuplicateDitado = false
+  const totalDitados = ditadosArray.length
+  for (let i = 0; i < totalDitados; i += 1) {
+    const ditado = random.getRandomDitado(false, ditadosArray)
+    if (ditado in seenDitados) {
+      foundDuplicateDitado = true
+      break
     }
-    expect(foundDuplicateDitado).toBe(false);
-});
+    seenDitados.push(ditado)
+  }
+  expect(foundDuplicateDitado).toBe(false)
+})
