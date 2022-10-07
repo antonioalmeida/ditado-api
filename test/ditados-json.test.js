cb2f97c15d6b92e0bd9316b04625a12b14ae0f55
Arnold Chand <creativenull@outlook.com>
2022-10-07 11:03:54 -0300

    fix: move require to top

--- test/ditados-json.test.js
+++ test/ditados-json.test.js
@@ -1,10 +1,10 @@
 const { expect, test } = require('@jest/globals')
 const Ajv2020 = require('ajv/dist/2020')
+const ditadosSchemaJson = require('../ditados.schema.json')
+const ditadosJson = require('../ditados.json')
 
 test('validate the ditados json file', () => {
   const ajv = new Ajv2020()
-  const ditadosSchemaJson = require('../ditados.schema.json')
-  const ditadosJson = require('../ditados.json')
   const validate = ajv.compile(ditadosSchemaJson)
 
   expect(validate(ditadosJson)).toBe(true)
