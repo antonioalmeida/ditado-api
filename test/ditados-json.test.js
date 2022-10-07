aff9d6e2d8d6ce2a036cf79bce20187f918d29e4
Arnold Chand <chand.arnold@hotmail.com>
2022-10-07 06:10:24 -0400

    Validate the ditados.json file on GH actions (#55)
    * feat: use ajv to validate json schema
    
    * feat: json schema file
    
    * feat: test for valid json file with a schema

---
+++ test/ditados-json.test.js
@@ -0,0 +1,11 @@
+const { expect, test } = require('@jest/globals')
+const Ajv2020 = require('ajv/dist/2020')
+
+test('validate the ditados json file', () => {
+  const ajv = new Ajv2020()
+  const ditadosSchemaJson = require('../ditados.schema.json')
+  const ditadosJson = require('../ditados.json')
+  const validate = ajv.compile(ditadosSchemaJson)
+
+  expect(validate(ditadosJson)).toBe(true)
+})
