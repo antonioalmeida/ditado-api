d6781904dfec7a8a3319e02bc9000614076c9a64
Henrique Vasconcelos <henriquetguedes@gmail.com>
2022-11-16 23:14:27 +0000

    Mastodon daily ditado tooter (#79)
    * added a ditado and corrected a typo
    
    * schedule log
    
    * lint correction
    
    * mastodon tooter
    
    * wrapped ditado in tooter
    
    * updated schedule for 0930 server time
    
    * PR changes tooter module and comment clarity
    
    Co-authored-by: António Almeida <theantonioalmeida@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const jsonServer = require('json-server')
+const schedule = require('node-schedule')
+require('dotenv').config()
+
 const generateDitado = require('./generate')
+const tooter = require('./tooter')
 const halves = require('./halves.json')
 
 const router = jsonServer.router('ditados.json')
@@ -29,3 +33,10 @@ server.use(router)
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+if (process.env.MAST_TOKEN) { // assumes that the correct Mastodon URL is also set if a token is provided, uses default masto.pt nevertheless
+  schedule.scheduleJob('30 9 * * *', () => {
+    const ditado = db.get('ditados').sample().value()
+    tooter(`Ditado do dia: \n\n--${ditado.content}\n\nSignificado: ${ditado.meaning}`)
+  })
+}
